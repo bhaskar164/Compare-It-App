f8e4f248c184741c83f19f478f2b85c0c50aa5ce
bhaskar164 <himanshubhaskar164@gmail.com>
2021-12-20 22:09:36 +0530

    getting input from frontend to the backend

--- src/main/java/com/projects/stringmatcher/controllers/MainController.java
+++ src/main/java/com/projects/stringmatcher/controllers/MainController.java
@@ -1,8 +1,10 @@
 package com.projects.stringmatcher.controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 @RequestMapping("/")
@@ -10,6 +12,13 @@ public class MainController {
 
 	@RequestMapping(method = RequestMethod.GET, value= "/home")
 	public String homePage() {
+		return "index";
+	}
+	
+	@PostMapping(value="/compare")
+	public String compareData(@RequestParam("fileOne") String data1, @RequestParam("fileTwo") String data2  ) {
+		System.out.println(data1);
+		System.out.println(data2);
 		return "index";
 	}
 }
