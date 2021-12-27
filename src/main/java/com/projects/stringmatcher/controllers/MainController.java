df737f9c542da3a86e16f4c3cbc14b34d6fa6fdd
bhaskar164 <himanshubhaskar164@gmail.com>
2021-12-28 02:18:37 +0530

    added highlight difference logic

--- src/main/java/com/projects/stringmatcher/controllers/MainController.java
+++ src/main/java/com/projects/stringmatcher/controllers/MainController.java
@@ -19,21 +19,23 @@ public class MainController {
 	@Autowired
 	ApplicationServices businessLogic;
 
-	@RequestMapping(method = RequestMethod.GET, value = "/home")
-	public String homePage() {
+	@RequestMapping(method = RequestMethod.GET, value = "/")
+	public String homePage(Model model) {
+		InputData inputData = new InputData();
+		model.addAttribute("data",inputData);
 		return "index";
 	}
 
 	@PostMapping(value = "/compare")
 	public String compareData(Model model,@ModelAttribute("data") InputData inputData) {
-		String data1 = inputData.getDuplicateString();
-		String data2 = inputData.getOriginalString();
+		String data1 = inputData.getOriginalString();
+		String data2 = inputData.getDuplicateString();
 		System.out.println(data1);
 		System.out.println(data2);
-		int p1 = businessLogic.compareStrings(data1, data2);
-		model.addAttribute("startPointer", p1);
+		String resultString= businessLogic.compareStrings(data1, data2);
+//		model.addAttribute("startPointer", p1);
 		model.addAttribute("data1", data1);
-		model.addAttribute("data2", data2);
+		model.addAttribute("data2", resultString);
 		return "index";
 	}
 }
--- src/main/java/com/projects/stringmatcher/services/ApplicationServices.java
+++ src/main/java/com/projects/stringmatcher/services/ApplicationServices.java
@@ -5,40 +5,43 @@ import java.util.ArrayList;
 import org.springframework.stereotype.Service;
 
 @Service
-public class ApplicationServices{
+public class ApplicationServices {
 
-	public int compareStrings(String original, String toCompare) {
+	public String compareStrings(String original, String toCompare) {
 		ArrayList<Integer> unmatchedIndeces = null;
 		if (original != null && toCompare != null) {
 			unmatchedIndeces = new ArrayList<>();
 //			int st=0;
 //			int end=0;
-			for(int i=0,j=0;i<original.length() && j<toCompare.length();i++,j++) {
-//				for(int j=0;j<toCompare.length();j++) {
-//					if(original.charAt(i+j) == toCompare.charAt(j)){
-//						/*
-//						 * O:- Elski Simba
-//						 * D:- elski simba
-//						 * 
-//						 */						
-//						}
-				if(original.charAt(i) != toCompare.charAt(j)) {
+			for (int i = 0, j = 0; i < original.length() && j < toCompare.length(); i++, j++) {
+				if (original.charAt(i) != toCompare.charAt(j)) {
 					unmatchedIndeces.add(j);
 				}
 			}
-			for(Integer x: unmatchedIndeces) {
-				System.out.println("val: "+x);
+			for (Integer x : unmatchedIndeces) {
+				System.out.println("val: " + x);
 			}
 		}
-		return -1;
+		return modifyStringUsingSpan(unmatchedIndeces, toCompare);
+	}
+
+	private String modifyStringUsingSpan(ArrayList<Integer> unmatchedIndeces, String toCompare) {
+		String resultString = "";
+		for (int i = 0; i < toCompare.length(); i++) {
+			if (unmatchedIndeces.contains(i)) {
+				resultString += "<span style=\"background-color:yellow;\">";
+				resultString += toCompare.charAt(i) + "</span>";
+			} else {
+				resultString += toCompare.charAt(i);
+			}
+
+		}
+		System.out.println("DATA:" + resultString);
+		return resultString;
 	}
 }
 
-
-
 /*
- * s1-> Far from home
- * s2-> next to home 
- * Approach 1:-
- * Compare every character of s1 with s2 
+ * s1-> Far from home s2-> next to home Approach 1:- Compare every character of
+ * s1 with s2
  */
